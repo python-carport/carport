c7ca862844f99f386293312b39b0ef9cb7c29365
151250215 <151250215@smail.nju.edu.cn>
2019-06-28 11:12:16 +0800

    test3

--- carport/data/t.py
+++ carport/data/t.py
@@ -2,18 +2,3 @@ import datetime
 
 print('s11s11a'.split('11'))
 print(datetime.datetime.now())
-
-def date_diff(begin_time, end_time):
-	diff = end_time - begin_time
-	diff_str = str(diff)
-	if diff_str.find(' days, ') > 0:
-		d = int(diff_str.split(' days, ')[0])
-		h = diff.seconds/60/60
-		return d*24+h
-	elif diff_str.find(' day, ') > 0:
-		h = diff.seconds / 60 / 60
-		return 24 + h
-	else:
-		h = diff.seconds/60/60
-		return h
-
